e88c1ecf16d902fed279a3506273b326a4d76613
evg-zlg <evg.zalogin@gmail.com>
2022-12-16 16:08:10 +0500

    feat: add sass, add rules for eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
+import './App.scss';
 
 function App() {
     return (
-        <div className="content">
-            <h1 className="content__title">Hello online-store</h1>
-            <p className="content__descript">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptate dolor odit minima iste quis ullam suscipit culpa iusto labore ea aliquam veritatis ad ipsa quam excepturi nesciunt, fugit laborum doloremque? Debitis quia obcaecati ut incidunt provident tempore vero, quam itaque est libero tempora, accusantium quo fugiat repellat minus facere.</p>
+        <div className='content'>
+            <h1 className='content__title'>Hello online-store</h1>
+            <p className='content__descript'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptate dolor odit minima iste quis ullam suscipit culpa iusto labore ea aliquam veritatis ad ipsa quam excepturi nesciunt, fugit laborum doloremque? Debitis quia obcaecati ut incidunt provident tempore vero, quam itaque est libero tempora, accusantium quo fugiat repellat minus facere.</p>
         </div>
     );
 }
