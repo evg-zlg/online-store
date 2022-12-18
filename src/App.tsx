56e1d726c8660a06aee8bd86dca6fbeddd10493b
evg-zlg <evg.zalogin@gmail.com>
2022-12-18 20:19:11 +0500

    feat: add productPage, chancge config prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.scss';
+import ProductsPage from './components/productsPage/productsPage';
 
 function App() {
-    return (
-        <div className='content'>
-            <h1 className='content__title'>Hello online-store</h1>
-            <p className='content__descript'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti voluptate dolor odit minima iste quis ullam suscipit culpa iusto labore ea aliquam veritatis ad ipsa quam excepturi nesciunt, fugit laborum doloremque? Debitis quia obcaecati ut incidunt provident tempore vero, quam itaque est libero tempora, accusantium quo fugiat repellat minus facere.</p>
-        </div>
-    );
+  return (
+    <div className='content'>
+      <h1 className='content__title'>Hello online-store</h1>
+      <ProductsPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/productsPage/productsPage.tsx
@@ -0,0 +1,9 @@
+import './productsPage.scss';
+
+export default function ProductsPage() {
+  return (
+    <>
+      <h2>products</h2>
+    </>
+  );
+};
