ba6d570c2d99050ae9d7676bf165a7d14d588980
evg-zlg <evg.zalogin@gmail.com>
2022-12-20 12:50:53 +0500

    feat: add view-control block

--- src/components/productsPage/productsPage.tsx
+++ src/components/productsPage/productsPage.tsx
@@ -1,16 +1,22 @@
 import './productsPage.scss'
 import { products } from '../../data/data'
 import { ProductCard } from '../productCard/productCard'
+import { ProductViewControl } from '../productViewControl/productViewControl'
 
 export default function ProductsPage() {
   return (
     <section className="products-page">
       <aside className="products-page__filter">панель</aside>
-      <main className="products-page__products">
-        {products.map((product) => {
-          return <ProductCard key={product.id} product={product} />
-        })}
-      </main>
+      <div className="products-page__content">
+        <ProductViewControl
+          className={'products-page__view-control view-control'}
+        />
+        <div className="products-page__products">
+          {products.map((product) => {
+            return <ProductCard key={product.id} product={product} />
+          })}
+        </div>
+      </div>
     </section>
   )
 }
