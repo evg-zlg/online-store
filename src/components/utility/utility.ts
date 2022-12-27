ea445bf9062b5691642a0680f8ac7af59692cde1
evg-zlg <evg.zalogin@gmail.com>
2022-12-27 09:28:35 +0500

    fix: utility add new array

--- src/components/utility/utility.ts
+++ src/components/utility/utility.ts
@@ -1,10 +1,15 @@
 import { products } from '../../data/data'
 
 const getMinPrice = () => {
-  return products.sort((a, b) => a.price - b.price)[0].price
+  const newProducts = []
+  newProducts.push(...products)
+  return newProducts.sort((a, b) => a.price - b.price)[0].price
 }
 const getMaxPrice = () => {
-  return products.sort((a, b) => a.price - b.price)[products.length - 1].price
+  const newProducts = []
+  newProducts.push(...products)
+  return newProducts.sort((a, b) => a.price - b.price)[newProducts.length - 1]
+    .price
 }
 
 export { getMinPrice, getMaxPrice }
