aaf82499df82b757f59e684f566d2c2fd68ab07a
evg-zlg <evg.zalogin@gmail.com>
2022-12-29 09:15:12 +0500

    fix: add noreferrer for link with target blank

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -9,11 +9,13 @@ export default function Footer() {
             className="gitHub__evg"
             href="https://github.com/evg-zlg"
             target="_blank"
+            rel="noreferrer"
           ></a>
           <a
             className="gitHub__kar"
             href="https://github.com/Karinaguseva"
             target="_blank"
+            rel="noreferrer"
           ></a>
         </div>
         <div className="footer__year">&copy; 2022</div>
@@ -21,6 +23,7 @@ export default function Footer() {
           className="footer__rss"
           href="https://rs.school/js/"
           target="_blank"
+          rel="noreferrer"
         ></a>
       </div>
     </footer>
