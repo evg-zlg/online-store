8e9ea0f1d95acd3204ed0951e8aec4693c772999
evg-zlg <evg.zalogin@gmail.com>
2023-01-17 15:22:14 +0500

    feat: add tests for data values and some functions

--- src/utility/utility.test.ts
+++ src/utility/utility.test.ts
@@ -1,19 +1,52 @@
-import { deleteParam, getMaxPrice, getMinPrice } from './utility'
-import { IProduct } from '../types'
+import {
+  deleteParam,
+  getMaxPrice,
+  getMinPrice,
+  getElementNameByIndex,
+  deduplicateArray,
+  changeBannerIndex,
+} from './utility';
+import { IProduct } from '../types';
 
-//deleteParam function for change query params
+// ===========================================
 
 test('delete "3" from "0.3.4"', () => {
-  expect(deleteParam('3', '0.3.4')).toEqual('0.4')
-})
+  expect(deleteParam('3', '0.3.4')).toEqual('0.4');
+});
 
 test('delete "0" from "0"', () => {
-  expect(deleteParam('0', '0')).toEqual('')
-})
+  expect(deleteParam('0', '0')).toEqual('');
+});
 
 test('delete "0" from "1.2"', () => {
-  expect(deleteParam('0', '1.2')).toEqual('1.2')
-})
+  expect(deleteParam('0', '1.2')).toEqual('1.2');
+});
+
+// ===========================================
+
+test('get tag 1 from tags', () => {
+  expect(getElementNameByIndex(1, 'tags')).toEqual('идея подарка');
+});
+test('get category 0 from categoryes', () => {
+  expect(getElementNameByIndex(0, 'categories')).toEqual('часы');
+});
+test('get not exist category', () => {
+  expect(getElementNameByIndex(-1, 'categories')).toBeFalsy();
+});
+test('get not exist tag', () => {
+  expect(getElementNameByIndex(-1, 'tags')).toBeFalsy();
+});
+
+// ===========================================
+
+test('change banner index from 0 to 1', () => {
+  expect(changeBannerIndex(0)).toEqual(1);
+});
+test('change banner index from 1 to 0', () => {
+  expect(changeBannerIndex(1)).toEqual(0);
+});
+
+// ===========================================
 
 //getMaxPrice from produtcs array
 
@@ -51,7 +84,7 @@ const produtcsArray1: IProduct[] = [
     count: 4,
     tags: ['', '', ' '],
   },
-]
+];
 const produtcsArray2: IProduct[] = [
   {
     id: 1,
@@ -62,7 +95,7 @@ const produtcsArray2: IProduct[] = [
     price: 100,
     images: [''],
     count: 4,
-    tags: ['', '', ' '],
+    tags: ['1', '2', '2', '3'],
   },
   {
     id: 2,
@@ -86,25 +119,45 @@ const produtcsArray2: IProduct[] = [
     count: 4,
     tags: ['', '', ' '],
   },
-]
-const productsArrayEmpty: IProduct[] = []
+];
+const productsArrayEmpty: IProduct[] = [];
+
+test('deduplicated list of products', () => {
+  const duplicatedArray: IProduct[] = [];
+  duplicatedArray.push(...produtcsArray1, ...produtcsArray1);
+
+  const checkDuplicate = (arr: IProduct[]) => {
+    let set = new Set();
+    let result = true;
+    arr.forEach((el) => {
+      if (set.has(el.id)) {        
+        result = false;
+      } else {
+        set.add(el.id);
+      }
+    });
+    return result;
+  };
+
+  expect(checkDuplicate(deduplicateArray(duplicatedArray))).toEqual(true);
+});
 test('find max price in the produtcsArray1', () => {
-  expect(getMaxPrice(produtcsArray1)).toEqual(300)
-})
+  expect(getMaxPrice(produtcsArray1)).toEqual(300);
+});
 test('find max price in the produtcsArray2', () => {
-  expect(getMaxPrice(produtcsArray2)).toEqual(100)
-})
+  expect(getMaxPrice(produtcsArray2)).toEqual(100);
+});
 test('find max price in the productsArrayEmpty', () => {
-  expect(getMaxPrice(productsArrayEmpty)).toEqual(0)
-})
+  expect(getMaxPrice(productsArrayEmpty)).toEqual(0);
+});
 
 //getMinPrice from produtcs array
 test('find min price in the produtcsArray1', () => {
-  expect(getMinPrice(produtcsArray1)).toEqual(100)
-})
+  expect(getMinPrice(produtcsArray1)).toEqual(100);
+});
 test('find min price in the produtcsArray2', () => {
-  expect(getMinPrice(produtcsArray2)).toEqual(100)
-})
+  expect(getMinPrice(produtcsArray2)).toEqual(100);
+});
 test('find min price in the productsArrayEmpty', () => {
-  expect(getMinPrice(productsArrayEmpty)).toEqual(0)
-})
+  expect(getMinPrice(productsArrayEmpty)).toEqual(0);
+});
