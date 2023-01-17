8da804c97f687384f686361358200d79cd3404fd
evg-zlg <evg.zalogin@gmail.com>
2023-01-17 10:22:08 +0500

    fix: add comment for eslint disable emptry array into useffect

--- src/components/categoryItem/categoryItem.tsx
+++ src/components/categoryItem/categoryItem.tsx
@@ -1,9 +1,9 @@
-import './categoryItem.scss'
-import { IProduct } from '../../types'
-import { useSearchParams } from 'react-router-dom'
-import { products } from '../../data/data'
-import { deleteParam } from '../utility/utility'
-import { useState, useEffect } from 'react'
+import './categoryItem.scss';
+import { IProduct } from '../../types';
+import { useSearchParams } from 'react-router-dom';
+import { products } from '../../data/data';
+import { deleteParam } from '../utility/utility';
+import { useState, useEffect } from 'react';
 
 interface ICategoryItemProps {
   item: string;
@@ -18,12 +18,12 @@ export const CategoryItem = ({
   type,
   index,
 }: ICategoryItemProps) => {
-  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams, setSearchParams] = useSearchParams();
   const [checked, setChecked] = useState(
     searchParams.get(type)?.includes(String(index)) || false,
-  )
-  const [productCurrentCount, setProductCurrentCount] = useState(0)
-  const [productTotalCount, setProductTotalCount] = useState(0)
+  );
+  const [productCurrentCount, setProductCurrentCount] = useState(0);
+  const [productTotalCount, setProductTotalCount] = useState(0);
   useEffect(() => {
     setProductCurrentCount(() => {
       return type === 'categories'
@@ -32,17 +32,17 @@ export const CategoryItem = ({
           ).length
         : productCurrent.filter((productCurrent) =>
             productCurrent.tags.join(' ').includes(item),
-          ).length
-    })
-  }, [item, type, productCurrent])
+          ).length;
+    });
+  }, [item, type, productCurrent]);
   useEffect(() => {
     setProductTotalCount(() => {
       return type === 'categories'
         ? products.filter((product) => product.category === item).length
         : products.filter((product) => product.tags.join(' ').includes(item))
-            .length
-    })
-  }, [item, type])
+            .length;
+    });
+  }, [item, type]);
   function clickCheckboxHandler() {
     const url = new URL(window.location.href);
     if (searchParams.has(type)) {
